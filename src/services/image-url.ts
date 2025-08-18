907f0491dbd23a96acdc9be3ab4b5dda9f8a16a3
Mustafa Bahomaid <mustafabahomaid@gmail.com>
2025-08-18 22:01:47 +0300

    feat(utils): add getCroppedImageUrl helper for optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,9 @@
+const getCroppedImageUrl = (url: string) => {
+    
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  url = url.slice(0, index) + 'crop/600/400/' + url.slice(index);
+
+  return url;
+};
+export default getCroppedImageUrl;
