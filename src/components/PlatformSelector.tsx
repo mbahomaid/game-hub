1613bda46ba6bfccd6bf021d27fab020e429b615
Mustafa Bahomaid <mustafabahomaid@gmail.com>
2025-08-21 01:25:27 +0300

    refactor(hooks): remove duplicate interfaces

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,5 +1,4 @@
-import { Platform } from "@/hooks/useGames";
-import usePlatforms from "@/hooks/usePlatforms";
+import usePlatforms, { Platform } from "@/hooks/usePlatforms";
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
