4e5c9048a151e75b5df5b50aa17016eb1e5e0a92
Mustafa Bahomaid <mustafabahomaid@gmail.com>
2025-08-23 22:25:37 +0300

    feat(games): implement infinite queries with React Query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,8 @@
 import { GameQuery } from "@/App";
 import { Platform } from "@/hooks/usePlatforms";
 import APIClient, { FetchResponse } from "@/services/api-client";
-
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { all } from "axios";
 
 const apiClient = new APIClient<Game>('/games');
 
@@ -19,18 +19,22 @@ export interface Game {
 
 
 const useGames = (gameQuery: GameQuery) =>
-  useQuery<FetchResponse<Game>, Error>({
+  useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ['games', gameQuery],
-    queryFn: () =>
+    queryFn: ({ pageParam = 1 }) =>
       apiClient
         .getAll({
           params: {
             genres: gameQuery.genre?.id,
             parent_platforms: gameQuery.platform?.id,
             ordering: gameQuery.sortOrder,
-            search: gameQuery.searchText
+            search: gameQuery.searchText,
+            page: pageParam
           },
-        })
+        }),
+        getNextPageParam: (lastPage, allPages) => {
+          return lastPage.next ? allPages.length + 1 : undefined;
+        }
   });
   
 export default useGames;
