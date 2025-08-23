00225ab24d6b47210ed06cdf1e1e7ea323233a70
Mustafa Bahomaid <mustafabahomaid@gmail.com>
2025-08-24 00:02:13 +0300

    refactor(hooks): simplify time calculations using ms library

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,6 +2,7 @@ import { GameQuery } from "@/App";
 import { Platform } from "@/hooks/usePlatforms";
 import APIClient, { FetchResponse } from "@/services/api-client";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
 
 const apiClient = new APIClient<Game>('/games');
 
@@ -34,7 +35,7 @@ const useGames = (gameQuery: GameQuery) =>
         getNextPageParam: (lastPage, allPages) => {
           return lastPage.next ? allPages.length + 1 : undefined;
         },
-        staleTime: 24 * 60 * 60 * 1000
+        staleTime: ms('24h')
   });
   
 export default useGames;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,7 @@
 import platforms from '@/Data/platforms';
 import APIClient from "@/services/api-client";
 import { useQuery } from '@tanstack/react-query';
+import ms from 'ms';
 
 const apiClient = new APIClient<Platform>('/platforms/lists/parents');
 
@@ -13,7 +14,7 @@ export interface Platform {
 const usePlatforms = () =>  useQuery({
   queryKey: ['platforms'],
   queryFn: apiClient.getAll,
-  staleTime: 24 * 60 * 60 * 1000,
+  staleTime: ms('24h'),
   initialData: platforms
 })
 
